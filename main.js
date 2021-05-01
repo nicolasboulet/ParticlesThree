48ef0d2bfd3daacaa57afe2d5e7842c31b8f0fee
Nicolas Boulet <nicolasboulet@live.fr>
2021-05-01 11:47:50 +0200

    sprite on cube

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import { Scene, WebGLRenderer, AxesHelper,
-PerspectiveCamera } from "three";
+         PerspectiveCamera, Mesh, BoxBufferGeometry,
+         MeshNormalMaterial, Points, PointsMaterial } from "three";
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 import './style.css'
 
 const scene = new Scene();
@@ -20,18 +24,53 @@ camera.position.y = 0.5;
 scene.add(camera);
 
 // render
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({
+  antialias: true,
+  });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 document.body.appendChild(renderer.domElement);
 
-//render the scene this the camera
+// control the vue in debug
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+//make a cube
+
+const cubeGeometry = new BoxBufferGeometry(1, 1, 1);
+const pointMaterial = new PointsMaterial({
+  color: 0xff0000,
+  size: 0.1,
+})
+const points = new Points(cubeGeometry, pointMaterial);
+
+// const cube = new Mesh(
+//     new BoxBufferGeometry(1, 1, 1),
+//     new MeshNormalMaterial()
+//   )
+
+
+scene.add(points);
 
 //make constant render
-const tick = () => {
+// and render the scene this the camera
+function tick() {
   renderer.render(scene, camera);
+  controls.update();
+//  camera.position.x += 0.01;
   requestAnimationFrame(tick);
 }
 
 tick();
+
+// resize the screen following the change of window
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  //update the camera
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
+
